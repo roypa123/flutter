0fd4dbc1bbac4b8907dfa592f1e58d5b4805affa
Roy P A <79005793+roypa123@users.noreply.github.com>
2022-01-04 10:57:02 +0530

    Create route_generator.dart
    a1

---
+++ route/route_generator.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:annama/main.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings){
+      final args = settings.arguments;
+
+      switch(settings.name){
+        case '/':
+           return MaterialPageRoute(builder: (_) => FirstPage());
+        case '/second':
+           if(args is String){
+             return MaterialPageRoute(
+               builder: (_) => SecondPage(
+                    data: args,
+               ),
+               );
+           }  
+
+           return _errorRoute();
+        default:
+           return _errorRoute();
+      }
+  }
+
+  static Route<dynamic> _errorRoute(){
+    return MaterialPageRoute(builder:(_){
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Error'),
+        ),
+        body: Center(
+          child: Text('ERROR'),
+          ),
+      );
+    }
+    );
+  }
+  
+
+}
